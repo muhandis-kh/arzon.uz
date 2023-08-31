8635ef2290005b0f50bf49459d191bc691153b12
okh-engineer <ohojimirzayev@gmail.com>
2023-08-31 12:29:03 +0500

    Add function for sello.uz

--- arzon2.py
+++ arzon2.py
@@ -1,14 +1,8 @@
-from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
-import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from urllib.parse import quote
 from pprint import pprint
+
+from colorama import Fore, Style
 
 mahsulot_nomi = input("Mahsulot nomini kiriting: ")
 encoded_query = quote(mahsulot_nomi)
@@ -38,19 +32,69 @@ def zoodmall():
         
     
         data = response.json()
-        pprint(data['marketList'][0:5])
+        products = data['marketList'][0:5]
+        API = []
+        for product in products:
+            API.append(
+                {
+                    'name': product['name'],
+                    'price': product['totalLocalPrice'],
+                    'link': f"https://www.zoodmall.uz/product/{product['productId']}/",
+                    'image_link': product['imgUrl']
+                }
+            )
+        
+        def get_price(API):
+            price = API.get('price', '0')
+            return float(price)
+        
+        API.sort(key=get_price, reverse=False)
+        pprint(API)
+        print(Fore.GREEN + "***********************************************************************************************************************")
+        print(Style.RESET_ALL)
     else:
         print("İstek başarısız oldu. Durum kodu:", response.status_code)
-
         
 def sello():
-
-    response = requests.get(f"https://stg-api.sello.uz/algolia/search?query={encoded_query}&page=1&perPage=30")
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+        "Fingerprint": "10b4ad6ce60f51511b502f34c14f8966",
+        "Language": "uz" 
+    }
+    response = requests.get(f"https://stg-api.sello.uz/algolia/search?query={encoded_query}&page=1&perPage=30", headers=headers)
     
     if response.status_code == 200:
         
     
         data = response.json()
-        pprint(data['marketList'][0:5])
+
+        products = data['hits'][0:5]
+        API = []
+        for product in products:
+            
+            if product['discounted_price']:
+                price = product['discounted_price']
+            else:
+                price = product['price']
+            API.append(
+                {
+                    'name': product['name'],
+                    'price': price,
+                    'link': f"https://sello.uz/uz/product/{product['slug']}/",
+                    'image_link': f"https://static.sello.uz/unsafe/x500/https://static.sello.uz{product['imageURL']}"
+                }
+            )
+        
+        def get_price(API):
+            price = API.get('price', '0')
+            return float(price)
+        
+        API.sort(key=get_price, reverse=False)
+        pprint(API)
     else:
         print("İstek başarısız oldu. Durum kodu:", response.status_code)
+
+
+zoodmall()
+sello()
