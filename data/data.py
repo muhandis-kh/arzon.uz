98b5d9367bcd8ac5caba989532b8432f8de99dad
okh-engineer <ohojimirzayev@gmail.com>
2023-09-14 23:04:46 +0500

    add dotenv package to project for sites api link's security

---
+++ data/data.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+zoodmall_api_link = os.getenv('zoodmall_api_link')
+sello_api_link = os.getenv('sello_api_link')
+olcha_api_link = os.getenv('olcha_api_link')
+texnomart_api_link = os.getenv('texnomart_api_link')
