a7b4fd90fa246eb6b929239f8397efa6ff4ef08a
okh-engineer <ohojimirzayev@gmail.com>
2023-11-07 17:30:45 +0500

    Update code

--- arzon_app/API/views.py
+++ arzon_app/API/views.py
@@ -21,13 +21,17 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminUserOrReadOnly
 from .throttles import CustomBearerTokenRateThrottle
+import os
 
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
-browser = webdriver.Chrome(options=chrome_options)
-
+# browser = webdriver.Chrome(options=chrome_options)
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
   
