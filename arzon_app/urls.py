5b3bf8ac05b0c0ffb24a2b5e53f27c706b5b6255
okh-engineer <ohojimirzayev@gmail.com>
2023-11-11 20:51:10 +0500

    Reverse code

--- arzon_app/urls.py
+++ arzon_app/urls.py
@@ -6,11 +6,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index),
-    path('home', home,name="home"),
     path('search-product/', SearchProductView.as_view(), name='search-product'),
     path('register/', UserRegistrationView.as_view(), name='register'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
