5421ded23237c4481479d18d510991c6fe96ef05
okh-engineer <ohojimirzayev@gmail.com>
2023-11-11 20:45:40 +0500

    Update code with vercel_deployment repo

--- arzon_app/urls.py
+++ arzon_app/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from arzon_app.API.views import SearchProductView, UserRegistrationView
+from arzon_app.views import index, home
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', index),
+    path('home', home,name="home"),
     path('search-product/', SearchProductView.as_view(), name='search-product'),
     path('register/', UserRegistrationView.as_view(), name='register'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
