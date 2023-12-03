1aea13e9b62307f7d38b458ade741bc2c2363afc
okh-engineer <ohojimirzayev@gmail.com>
2023-12-03 18:28:23 +0500

    Update staticfiles_storage

--- core/settings.py
+++ core/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 ROOT_URLCONF = 'core.urls'
 
