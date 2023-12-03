ecb9c98912bef5a3a4d5d34b036d883765777318
okh-engineer <ohojimirzayev@gmail.com>
2023-12-03 18:12:22 +0500

    Change debug = False

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = django_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', '.onrender.com', '.pythonanywhere.com', 'arzonuz.up.railway.app']
 
